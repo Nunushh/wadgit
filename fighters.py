1699a00b54994a067b71463f5661a7330591d9f1
Nur Guclu <nur.guclu92@gmail.com>
2020-06-24 14:42:22 +0200

    add a nex file

---
+++ fighters.py
@@ -0,0 +1,20 @@
+fighter1 = {
+"name" : "Rocky la tomate",
+"poing" : 6, 
+"pied" : 5, 
+"morsure" : 3, 
+"life" : 10,
+"defense" : 1, }
+
+
+fighter2 = {
+"name" : "Carotte hargneuse", 
+"poing" : 4, 
+"pied" : 6, 
+"morsure" : 3,
+"life" : 10,
+"defense" : 2 }
+
+attacks =["poing", "pied", "morsure"]
+
+    
